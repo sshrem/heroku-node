aaaf0b579d071915395beec04d30ea4b23c7b246
Shimon Shrem <sshrem@obil-shrem.il.outbrain.com>
2016-11-08 18:12:02 +0200

    change prod port

--- server/config/env/prod.js
+++ server/config/env/prod.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     env: 'prod',
-    port: 4020,
+    port: 80,
     loadProjectRequestUrl: 'http://project-services.herokuapp.com/api/project',
     loadDesignsRequestUrl: 'http://project-services.herokuapp.com/api/designs'
 };
