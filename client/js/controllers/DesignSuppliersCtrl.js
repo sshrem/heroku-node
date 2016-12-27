64982a68a1f1aeb30f1e8d0a0d3810b3084d66a6
sshrem <qaz123wsx>
2016-12-27 02:10:13 +0200

    new video layout

--- client/js/controllers/DesignSuppliersCtrl.js
+++ client/js/controllers/DesignSuppliersCtrl.js
@@ -1,13 +1,16 @@
 angular.module('DisignStudio')
   .controller('DesignSuppliersCtrl', function ($rootScope, $scope, Cloudinary, designByRoomFilter, $window, $http,
-                                               $stateParams, debugData, $timeout) {
+                                               $stateParams, debugData, $timeout, $location, $anchorScroll) {
 
     var initRequestUrl = 'http://' + $rootScope.domain + '/api/designsFilters';
 
+    var cloudinaryPath= "projects/shikunbinuidpchk/";
+    var cloudinaryPath2= "video:projects:shikunbinuidpchk:";
+
     $scope.roomItems;
-    $scope.itemFilters = [];
     $scope.allSuppliers;
-    $rootScope.selected = [];
+    $scope.showVideoDiv = false;
+    $scope.videoList=[];
 
     $(document).ready(function () {
       $('.tooltipped').tooltip({delay: 50});
@@ -19,21 +22,51 @@ angular.module('DisignStudio')
       $('.collapsible').collapsible();
     }, 1000);
 
-    $scope.getDesignsParams = function () {
-      var params = {
-        projId: $stateParams.projId,
-        aptId: $stateParams.aptId,
-        itemFilters: $scope.itemFilters
-      };
-      return params;
-    }
+    $scope.getVideoValue = function (supplierId, roomId, offeringId){
+      var path = cloudinaryPath2 +  $stateParams.aptId + ":";
+      if (roomId == 2) {
+        path = cloudinaryPath +  $stateParams.aptId + "/" ;
+      }
+      path = path + supplierId + "_" + roomId + "_"  + offeringId;
 
-    $scope.getSelectedValue = function (roomId, offeringId) {
-      return {room: roomId, offer: offeringId}
-    }
+      return path;
 
-    $scope.playVideo = function () {
-      $('#video').get[0].start();
+    }
+    $scope.playDesignVideo = function () {
+      $scope.showVideoDiv=true;
+      var mp4Url = Cloudinary.url($scope.videoList[2], {resource_type: 'video', format: 'mp4',
+        transformation: [
+          {overlay: $scope.videoList[1], flags: "splice"},
+          {overlay: $scope.videoList[3], flags: "splice"},
+        ]});
+
+      var webmUrl = Cloudinary.url($scope.videoList[2], {resource_type: 'video', format: 'webm',
+        transformation: [
+          {overlay: $scope.videoList[1], flags: "splice"},
+          {overlay: $scope.videoList[3], flags: "splice"},
+        ]});
+
+      var oggUrl = Cloudinary.url($scope.videoList[2], {resource_type: 'video', format: 'ogg',
+        transformation: [
+          {overlay: $scope.videoList[1], flags: "splice"},
+          {overlay: $scope.videoList[3], flags: "splice"},
+        ]});
+
+      $('#video2 source[type="video/mp4"]' ).get(0).setAttribute("src", mp4Url);
+      $('#video2 source[type="video/ogg"]' ).get(0).setAttribute("src", oggUrl);
+      $('#video2 source[type="video/webm"]' ).get(0).setAttribute("src", webmUrl);
+      var currentTime = $('#video2').get(0).currentTime;
+      var duration = $('#video2').get(0).duration;
+
+      $('#video2').get(0).load();
+      if (currentTime>0 && currentTime<duration){
+        $('#video2').get(0).currentTime = currentTime;
+      }
+      $('#video2').get(0).play();
+      // $location.hash('video2');
+      // $anchorScroll();
+
+
     }
 
     $scope.getIcon = function (roomId) {
@@ -60,11 +93,7 @@ angular.module('DisignStudio')
         $scope.allSuppliers = $scope.data.suppliers;
         $scope.roomItems = $scope.data.roomItems;
         for (var room in $scope.roomItems) {
-          $rootScope.selected[room] = {
-            room: $scope.roomItems[room].roomId,
-            offer: $scope.roomItems[room].items[0].offeringId
-          };
-          $scope.itemFilters[room] = $scope.roomItems[room].roomId + "_" + $scope.roomItems[room].items[0].offeringId;
+          $scope.videoList[$scope.roomItems[room].roomId] = $scope.getVideoValue($scope.roomItems[room].items[0].supplierId, $scope.roomItems[room].roomId, $scope.roomItems[room].items[0].offeringId);
         }
 
       } else {
@@ -76,12 +105,7 @@ angular.module('DisignStudio')
             $scope.allSuppliers = res.data.suppliers;
             $scope.roomItems = res.data.roomItems;
             for (var room in $scope.roomItems) {
-              $rootScope.selected[room] = {
-                room: $scope.roomItems[room].roomId,
-                offer: $scope.roomItems[room].items[0].offeringId
-              };
-
-              $scope.itemFilters[room] = $scope.roomItems[room].roomId + "_" + $scope.roomItems[room].items[0].offeringId;
+              $scope.videoList[$scope.roomItems[room].roomId] = $scope.getVideoValue($scope.roomItems[room].items[0].supplierId, $scope.roomItems[room].roomId, $scope.roomItems[room].items[0].offeringId);
             }
           }
 
