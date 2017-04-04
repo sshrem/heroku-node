df095f3d6176a7b486203ae62d6ede4b573f187c
Shimon Shrem <s.shrem@f5.com>
2017-04-05 00:40:29 +0300

    1. facebook share url 2. watch room selector

--- client/js/controllers/DesignSuppliersCtrl.js
+++ client/js/controllers/DesignSuppliersCtrl.js
@@ -3,16 +3,33 @@ angular.module('DisignStudio')
                                                $stateParams, debugData, $timeout, $location, $anchorScroll) {
 
     var initRequestUrl = 'http://' + $rootScope.domain + '/api/designsFilters';
+    $scope.designRequestUrl = 'http://' + $rootScope.domain + '/api/designs';
 
-    var cloudinaryPath= "projects/shikunbinuidpchk/";
-    var cloudinaryPath2= "video:projects:shikunbinuidpchk:";
+    var cloudinaryPath = "projects/shikunbinuidpchk/";
+    var cloudinaryPath2 = "video:projects:shikunbinuidpchk:";
 
     $scope.roomItems;
     $scope.roomToWatch={
-      val: "test"
+      val: "-1"
     };
     $scope.allSuppliers;
-    $scope.videoList=[];
+    $scope.videoList = [];
+    $scope.roomOffering = [];
+    $scope.defaultFacebookVideoUrl;
+    $scope.facebookVideo = {url: null};
+    $scope.videoDetails=[];
+    $scope.videoLength=0
+    $scope.videoStartTime=0;
+    $scope.videoEndTime=0;
+
+    $('#video2').get(0).ontimeupdate = function(){
+      var currentTime = this.currentTime;
+      if (currentTime >= $scope.videoEndTime){
+        this.pause();
+      }
+
+    }
+
     $location.hash('apartment-nav');
     $anchorScroll();
 
@@ -26,54 +43,106 @@ angular.module('DisignStudio')
       $('.collapsible').collapsible();
     }, 1000);
 
-    $scope.getVideoValue = function (supplierId, roomId, offeringId){
-      var path = cloudinaryPath2 +  $stateParams.aptId + ":";
+    $scope.getVideoValue = function (supplierId, roomId, offeringId) {
+      $scope.roomOffering[roomId] = offeringId;
+      var path = cloudinaryPath2 + $stateParams.aptId + ":";
       if (roomId == 2) {
-        path = cloudinaryPath +  $stateParams.aptId + "/" ;
+        path = cloudinaryPath + $stateParams.aptId + "/";
       }
-      path = path + supplierId + "_" + roomId + "_"  + offeringId;
+      path = path + supplierId + "_" + roomId + "_" + offeringId;
 
       return path;
 
     }
 
-    $scope.changeRoomToWatch = function(index){
+    $scope.getFacebookVideoUrl = function () {
+      var filters = [];
+      for (var ro  in $scope.roomOffering) {
+        var room = parseInt(ro);
+        var offer = $scope.roomOffering[ro];
+        filters.push({room: room, offer: offer});
+      }
+
+      var data = {
+        atId: $stateParams.aptId,
+        projId: $stateParams.projId,
+        supplierFilter: {
+          supplier: null
+        },
+        itemFilters: filters
+      };
+      $http.post($scope.designRequestUrl, data)
+        .success(function (res) {
+          var url = null;
+          if (res.data && res.data.designs && res.data.designs.length>0) {
+            var url = res.data.designs[0].facebookVideoUrl;
+          }
+          if (url != null){
+            $scope.facebookVideo = {url: url};
+          } else {
+            $scope.facebookVideo = {url: $scope.defaultFacebookVideoUrl};
+          }
+        }).error(function (e) {
+        var a = 1;
+      });
+    };
+
+
+    $scope.changeRoomToWatch = function(roomId){
+      if (roomId==-1){
+        $scope.videoStartTime = 0;
+        $scope.videoEndTime = $scope.videoLength;
+      } else {
+        var video = $scope.videoDetails[roomId];
+        $scope.videoStartTime = video.startTime;
+        $scope.videoEndTime = video.endTime;
+      }
+      $scope.playDesignVideo();
     };
 
     $scope.changeDesignVideo = function(){
       var isPaused = $('#video2').get(0).paused;
       // if (!isPaused){
-        $scope.playDesignVideo();
+      $scope.playDesignVideo();
+      $scope.getFacebookVideoUrl()
       // }
 
     };
     $scope.playDesignVideo = function () {
-      var mp4Url = Cloudinary.url($scope.videoList[2], {resource_type: 'video', format: 'mp4',
+      var mp4Url = Cloudinary.url($scope.videoList[2], {
+        resource_type: 'video', format: 'mp4',
         transformation: [
           {overlay: $scope.videoList[1], flags: "splice"},
           {overlay: $scope.videoList[3], flags: "splice"},
-        ]});
+        ]
+      });
 
-      var webmUrl = Cloudinary.url($scope.videoList[2], {resource_type: 'video', format: 'webm',
+      var webmUrl = Cloudinary.url($scope.videoList[2], {
+        resource_type: 'video', format: 'webm',
         transformation: [
           {overlay: $scope.videoList[1], flags: "splice"},
           {overlay: $scope.videoList[3], flags: "splice"},
-        ]});
+        ]
+      });
 
-      var oggUrl = Cloudinary.url($scope.videoList[2], {resource_type: 'video', format: 'ogg',
+      var oggUrl = Cloudinary.url($scope.videoList[2], {
+        resource_type: 'video', format: 'ogg',
         transformation: [
           {overlay: $scope.videoList[1], flags: "splice"},
           {overlay: $scope.videoList[3], flags: "splice"},
-        ]});
+        ]
+      });
 
-      $('#video2 source[type="video/mp4"]' ).get(0).setAttribute("src", mp4Url);
-      $('#video2 source[type="video/ogg"]' ).get(0).setAttribute("src", oggUrl);
-      $('#video2 source[type="video/webm"]' ).get(0).setAttribute("src", webmUrl);
+      $('#video2 source[type="video/mp4"]').get(0).setAttribute("src", mp4Url);
+      $('#video2 source[type="video/ogg"]').get(0).setAttribute("src", oggUrl);
+      $('#video2 source[type="video/webm"]').get(0).setAttribute("src", webmUrl);
       var currentTime = $('#video2').get(0).currentTime;
-      var duration = $('#video2').get(0).duration;
+      if (currentTime < $scope.videoStartTime || currentTime> $scope.videoEndTime){
+        currentTime = $scope.videoStartTime;
+      }
 
       $('#video2').get(0).load();
-      if (currentTime>0 && currentTime<duration){
+      if (currentTime >= 0 && currentTime < $scope.videoEndTime) {
         $('#video2').get(0).currentTime = currentTime;
       }
       $('#video2').get(0).play();
@@ -97,28 +166,38 @@ angular.module('DisignStudio')
       return index == 0;
     }
 
+    function extractedData(data) {
+      $scope.allSuppliers = data.suppliers;
+      $scope.roomItems = data.roomItems;
+      $scope.defaultFacebookVideoUrl = data.defaultFacebookVideoUrl;
+
+      for (var room in $scope.roomItems) {
+        $scope.videoList[$scope.roomItems[room].roomId] = $scope.getVideoValue($scope.roomItems[room].items[0].supplierId, $scope.roomItems[room].roomId, $scope.roomItems[room].items[0].offeringId);
+        $scope.roomOffering[$scope.roomItems[room].roomId] = $scope.roomItems[room].items[0].offeringId;
+      }
+
+      for (var room in data.videosDetails){
+        var video = data.videosDetails[room];
+        $scope.videoDetails[video.roomId] = video;
+        $scope.videoLength = video.endTime;
+        $scope.videoEndTime = video.endTime;
+      }
+
+      $scope.getFacebookVideoUrl();
+    }
+
     function init() {
       if ($rootScope.isDebugMode) {
-        $scope.data = debugData.debugData.designSupplier;
-        $scope.allSuppliers = $scope.data.suppliers;
-        $scope.roomItems = $scope.data.roomItems;
-        for (var room in $scope.roomItems) {
-          $scope.videoList[$scope.roomItems[room].roomId] = $scope.getVideoValue($scope.roomItems[room].items[0].supplierId, $scope.roomItems[room].roomId, $scope.roomItems[room].items[0].offeringId);
-        }
-
+        var data = debugData.debugData.designSupplier;
+        extractedData(data);
       } else {
         $http.post(initRequestUrl, {
           atId: $stateParams.aptId,
           projId: $stateParams.projId
         }).success(function (res) {
           if (res.data) {
-            $scope.allSuppliers = res.data.suppliers;
-            $scope.roomItems = res.data.roomItems;
-            for (var room in $scope.roomItems) {
-              $scope.videoList[$scope.roomItems[room].roomId] = $scope.getVideoValue($scope.roomItems[room].items[0].supplierId, $scope.roomItems[room].roomId, $scope.roomItems[room].items[0].offeringId);
-            }
+            extractedData(res.data);
           }
-
         }).error(function (e) {
           var a = 1;
         });
--- client/js/services/DebugData.js
+++ client/js/services/DebugData.js
@@ -40,11 +40,21 @@ app.factory('debugData', function () {
         }]
       },
       project: {
-        "title": "חלומות פרדס חנה - כרכור",
-        "about": "<p>שיכון ובינוי נדלן ממשיכה את תנופת ההצלחה במושבה הירוקה ומזמינה אתכם לחוויית מגורים מיוחדת במינה.</p>\n<p> בואו ליהנות מפרויקט מתקדם בסטנדרט גבוה, הממוקם ליד ספריית היישוב ומציע גני שעשועים מתחת לבית, בתי ספר קרובים, מגוון רחב של אפשרויות תעסוקה קנייה ובילוי במרחק נסיעה קצרה והכל במרחק 3 דקות נסיעה מהרכבת ומהיציאה לכביש החוף ו - 6 דקות מכביש 6. </p>הפרויקט נבנה על פי עקרונות הבנייה הירוקה ומקבל תקן ישראלי ממכון התקנים. בפרויקט מתוכננים 8 בניינים, לרשותכם מגוון דירות רחב.",
+        "projectId": 3,
+        "code": "shikunbinuidpchk",
+        "name": "חלומות פרדס חנה - כרכור",
+        "about": "<p>שיכון ובינוי נדלן ממשיכה את תנופת ההצלחה במושבה הירוקה ומזמינה אתכם לחוויית מגורים מיוחדת במינה.</p>\n<p> בואו ליהנות מפרויקט מתקדם בסטנדרט גבוה, הממוקם ליד ספריית היישוב ומציע גני שעשועים מתחת לבית, בתי ספר קרובים, מגוון רחב של אפשרויות תעסוקה קנייה ובילוי במרחק נסיעה קצרה והכל במרחק 3 דקות נסיעה מהרכבת ומהיציאה לכביש החוף ו - 6 דקות מכביש 6. </p>הפרויקט נבנה על פי עקרונות הבנייה הירוקה ומקבל תקן ישראלי ממכון התקנים. בפרויקט מתוכננים 8 בניינים, לרשותכם מגוון דירות רחב",
+        "location": "נווה פרדסים",
+        "lon": 34.95089,
+        "lat": 32.469506,
         "logo": "projects/shikunbinuidpchk/project-logo.png",
-        "image": "projects/shikunbinuidpchk/background.jpg",
+        "imageUrl": "projects/shikunbinuidpchk/background",
+        "salesPhone": "074-7290057 ",
+        "countryId": 1,
+        "cityId": 1,
         "entrepreneurCachedData": {
+          "id": 3,
+          "name": "שיכון ובינוי",
           "logo": "entrepreneurs/shikunvebinui-logo.png",
           "about": "<p>שיכון ובינוי נדל”ן הינה חברת הנדל”ן הגדולה בישראל הנהנית מחוסן כלכלי ומצמיחה מתמדת.</p>\n\n<p>החברה הינה הזרוע היזמית של קבוצת שיכון ובינוי, שבשנת 2011 קבעו סקרי BDI ו-B&D כי היא חברת התשתיות והנדל”ן המובילה בישראל.</p>\n\n<p> החברה הוקמה לפני 90 שנה ועד היום בנתה מעל 185,000 יחידות דיור בכל רחבי הארץ ליותר ממיליון איש.\n מאז נוסדה, פועלת החברה מתוך חזון לבניית הארץ, ועושה זאת בהצלחה רבה ובהתאמה לצרכים המשתנים של האוכלוסייה.</p>\n\n<p> שיכון ובינוי נדל”ן מגשימה לאורך השנים חלומות של משפחות רבות באמצעות הקמת סביבת מגורים משפחתית מושלמת, ומקימה את שכונותיה החדשות בגישה נוחה לכבישים מרכזיים כמו כביש 6 ולתחנות רכבת, והופכת את מרכז הארץ לנגיש במיוחד. שיכון ובינוי נדל”ן מעודדת חיי קהילה פעילים בשכונותיה ומשתפת פעולה עם תושבי השכונה, עוד אחד מהיתרונות שהופכים את שכונות החלומות שלה למבוקשות ביותר בארץ.</p>\n\n <p>חברת שיכון ובינוי נדל”ן מובילה את הבנייה הירוקה בישראל, והיא הראשונה שקיבלה הלכה למעשה תו תקן ירוק 5281 לבנייה רוויה בגמר בנייה. כחלק מקבוצת שיכון ובינוי, דוגלת החברה בעקרונות הקיימות, ובכל הפרויקטים שלה ברחבי הארץ היא בונה עפ”י תקנים לבנייה ירוקה. האלמנטים הירוקים בפרויקט כוללים: צמחייה חסכונית במים בגינון, הכנה להפרדת סוגי פסולת, בידוד אקוסטי ותרמי משופר, זיגוג LOW-E בדירות הפונות לדרום, פאנל מרכזי בכניסה לדירה להדלקה וכיבוי של תאורה ומזגן, הכנה למאווררי תקרה בשני חדרים, מערכת להגנה מנזקי האבנית בצנרת אספקת המים, חסכמים בברזים והכנה לחימום מים בגז. </p>"
         },
@@ -64,130 +74,260 @@ app.factory('debugData', function () {
           "חניה צמודה לכל דירה",
           "מחסן לכל דירה"
         ],
-        "apartmentTemplateCachedData": [{
-          "code": 1,
-          "name": "דירת 5 חדרים | טיפוס A1",
-          "imgCode": "projects/shikunbinuidpchk/plan_A1"
-        }, {
-          "code": 2,
-          "name": "דירת 5 חדרים | טיפוס A2",
-          "imgCode": "projects/shikunbinuidpchk/plan_A2.png"
-        }, {
-          "code": 3,
-          "name": "דירת 4 חדרים | טיפוס B1",
-          "imgCode": "projects/shikunbinuidpchk/plan_B1.png"
-        }, {
-          "code": 4,
-          "name": "דירת 4 חדרים | טיפוס B2",
-          "imgCode": "projects/shikunbinuidpchk/plan_B2.png"
-        }, {
-          "code": 5,
-          "name": "מיני פנטהאוז 5 חדרים | טיפוס MPH1 ",
-          "imgCode": "projects/shikunbinuidpchk/plan_MPH1.png"
-        }, {
-          "code": 6,
-          "name": "מיני פנטהאוז 5 חדרים | טיפוס MPH2 ",
-          "imgCode": "projects/shikunbinuidpchk/plan_MPH2.png"
-        }, {
-          "code": 7,
-          "name": "מיני פנטהאוז 5 חדרים | טיפוס MPH3 ",
-          "imgCode": "projects/shikunbinuidpchk/plan_MPH3.png"
-        }, {
-          "code": 8,
-          "name": "פנטהאוז 5 חדרים | טיפוס PH1",
-          "imgCode": "projects/shikunbinuidpchk/plan_PH1.png"
-        }, {
-          "code": 9,
-          "name": "פנטהאוז 5 חדרים | טיפוס PH2",
-          "imgCode": "projects/shikunbinuidpchk/plan_PH2.png"
-        }]
-      },
-      designSupplier: {
-        "title": "דירת 4 חדרים | טיפוס B2",
-        "suppliers": [
+        "apartmentTemplateCachedData": [
           {
             "id": 1,
-            "name": null,
-            "logo": "suppliers/zehavi/zehavi-logo",
-            "url": null
-          }
-        ],
-        "roomItems": [
-          {
-            "title": "chooselivingroomfloor",
-            "roomId": 2,
-            "items": [
-              {
-                "offeringId": 5,
-                "name": "Imperial Crema 60x60",
-                "imgCode": "suppliers/zehavi/clearcrema.jpg",
-                "supplierId": 1
-              },
-              {
-                "offeringId": 6,
-                "name": "Manhatan Ivory 60x60",
-                "imgCode": "suppliers/zehavi/manhatanivory.jpg",
-                "supplierId": 1
-              },
-              {
-                "offeringId": 14,
-                "name": "Clear Tobbaco 60x60",
-                "imgCode": "suppliers/zehavi/cleartobbaco_.jpg",
-                "supplierId": 1
-              }
-            ]
-          }
-          ,
-          {
-            "title": "choosebedroomfloor",
-            "roomId": 1,
-            "items": [
-              {
-                "offeringId": 5,
-                "name": "Imperial Crema 60x60",
-                "imgCode": "suppliers/zehavi/clearcrema.jpg",
-                "supplierId": 1
-              },
-              {
-                "offeringId": 6,
-                "name": "Manhatan Ivory 60x60",
-                "imgCode": "suppliers/zehavi/manhatanivory.jpg",
-                "supplierId": 1
-              },
-              {
-                "offeringId": 13,
-                "name": "פרקט כרמל Classic Frontal",
-                "imgCode": "suppliers/zehavi/Carmel_Classic_Frontal.jpg",
-                "supplierId": 1
-              }
-            ]
+            "name": "דירת 5 חדרים | טיפוס A1",
+            "image": "projects/shikunbinuidpchk/A1/plan_A1.png",
+            "code": "A1",
+            "numOfRooms": 5,
+            "defaultFacebookVideoUrl": "https://www.facebook.com/1848414988734528/videos/1848855945357099/"
           },
           {
-            "title": "choosebathroomcladding",
+            "id": 2,
+            "name": "דירת 5 חדרים | טיפוס A2",
+            "image": "projects/shikunbinuidpchk/A2/plan_A2.png",
+            "code": "A2",
+            "numOfRooms": 5,
+            "defaultFacebookVideoUrl": "https://www.facebook.com/1848414988734528/videos/1848855945357099/"
+          },
+          {
+            "id": 3,
+            "name": "דירת 4 חדרים | טיפוס B1",
+            "image": "projects/shikunbinuidpchk/B1/plan_B1.png",
+            "code": "B1",
+            "numOfRooms": 4,
+            "defaultFacebookVideoUrl": "https://www.facebook.com/1848414988734528/videos/1848855945357099/"
+          },
+          {
+            "id": 4,
+            "name": "דירת 4 חדרים | טיפוס B2",
+            "image": "projects/shikunbinuidpchk/B2/plan_B2.png",
+            "code": "B2",
+            "numOfRooms": 4,
+            "defaultFacebookVideoUrl": "https://www.facebook.com/1848414988734528/videos/1848855945357099/"
+          }
+        ],
+        "videoDetails": [
+          {
+            "id": 23,
+            "projectId": 3,
+            "apartmentTemplateId": 1,
+            "roomId": 1,
+            "videoLength": 55
+          },
+          {
+            "id": 24,
+            "projectId": 3,
+            "apartmentTemplateId": 1,
+            "roomId": 2,
+            "videoLength": 45
+          },
+          {
+            "id": 25,
+            "projectId": 3,
+            "apartmentTemplateId": 1,
             "roomId": 3,
-            "items": [
-              {
-                "offeringId": 10,
-                "name": "Vetro Marfil 20x60",
-                "imgCode": "suppliers/zehavi/vetromarfil.jpg",
-                "supplierId": 1
-              },
-              {
-                "offeringId": 11,
-                "name": "Rain White 20x50",
-                "imgCode": "suppliers/zehavi/rainwhite.jpg",
-                "supplierId": 1
-              },
-              {
-                "offeringId": 12,
-                "name": "Ethos Gris 20x60",
-                "imgCode": "suppliers/zehavi/ethosgris.jpg",
-                "supplierId": 1
-              }
-            ]
+            "videoLength": 30
+          },
+          {
+            "id": 26,
+            "projectId": 3,
+            "apartmentTemplateId": 2,
+            "roomId": 1,
+            "videoLength": 60
+          },
+          {
+            "id": 27,
+            "projectId": 3,
+            "apartmentTemplateId": 2,
+            "roomId": 2,
+            "videoLength": 45
+          },
+          {
+            "id": 28,
+            "projectId": 3,
+            "apartmentTemplateId": 2,
+            "roomId": 3,
+            "videoLength": 30
+          },
+          {
+            "id": 29,
+            "projectId": 3,
+            "apartmentTemplateId": 3,
+            "roomId": 1,
+            "videoLength": 45
+          },
+          {
+            "id": 30,
+            "projectId": 3,
+            "apartmentTemplateId": 3,
+            "roomId": 2,
+            "videoLength": 45
+          },
+          {
+            "id": 31,
+            "projectId": 3,
+            "apartmentTemplateId": 3,
+            "roomId": 3,
+            "videoLength": 30
+          },
+          {
+            "id": 32,
+            "projectId": 3,
+            "apartmentTemplateId": 4,
+            "roomId": 1,
+            "videoLength": 45
+          },
+          {
+            "id": 33,
+            "projectId": 3,
+            "apartmentTemplateId": 4,
+            "roomId": 2,
+            "videoLength": 50
+          },
+          {
+            "id": 34,
+            "projectId": 3,
+            "apartmentTemplateId": 4,
+            "roomId": 3,
+            "videoLength": 30
           }
         ]
       },
+
+
+
+
+      designSupplier:{
+          "title": "דירת 4 חדרים | טיפוס B2",
+          "suppliers": [
+            {
+              "id": 1,
+              "name": "זהבי עצמון",
+              "logo": "suppliers/zehavi/zehavi-logo",
+              "url": "http://www.zahavi.co.il/"
+            }
+          ],
+          "roomItems": [
+            {
+              "title": "chooselivingroomfloor",
+              "roomId": 2,
+              "items": [
+                {
+                  "offeringId": 5,
+                  "name": "Imperial Crema 60x60",
+                  "imgCode": "suppliers/zehavi/clearcrema.jpg",
+                  "supplierId": 1
+                },
+                {
+                  "offeringId": 6,
+                  "name": "Manhatan Ivory 60x60",
+                  "imgCode": "suppliers/zehavi/manhatanivory.jpg",
+                  "supplierId": 1
+                },
+                {
+                  "offeringId": 13,
+                  "name": "פרקט כרמל Classic Frontal",
+                  "imgCode": "suppliers/zehavi/Carmel_Classic_Frontal.jpg",
+                  "supplierId": 1
+                },
+                {
+                  "offeringId": 14,
+                  "name": "Clear Tobbaco 60x60",
+                  "imgCode": "suppliers/zehavi/cleartobbaco_.jpg",
+                  "supplierId": 1
+                }
+              ]
+            },
+            {
+              "title": "choosebedroomfloor",
+              "roomId": 1,
+              "items": [
+                {
+                  "offeringId": 5,
+                  "name": "Imperial Crema 60x60",
+                  "imgCode": "suppliers/zehavi/clearcrema.jpg",
+                  "supplierId": 1
+                },
+                {
+                  "offeringId": 6,
+                  "name": "Manhatan Ivory 60x60",
+                  "imgCode": "suppliers/zehavi/manhatanivory.jpg",
+                  "supplierId": 1
+                },
+                {
+                  "offeringId": 13,
+                  "name": "פרקט כרמל Classic Frontal",
+                  "imgCode": "suppliers/zehavi/Carmel_Classic_Frontal.jpg",
+                  "supplierId": 1
+                },
+                {
+                  "offeringId": 14,
+                  "name": "Clear Tobbaco 60x60",
+                  "imgCode": "suppliers/zehavi/cleartobbaco_.jpg",
+                  "supplierId": 1
+                }
+              ]
+            },
+            {
+              "title": "choosebathroomcladding",
+              "roomId": 3,
+              "items": [
+                {
+                  "offeringId": 10,
+                  "name": "Vetro Marfil 20x60",
+                  "imgCode": "suppliers/zehavi/vetromarfil.jpg",
+                  "supplierId": 1
+                },
+                {
+                  "offeringId": 11,
+                  "name": "Rain White 20x50",
+                  "imgCode": "suppliers/zehavi/rainwhite.jpg",
+                  "supplierId": 1
+                },
+                {
+                  "offeringId": 12,
+                  "name": "Ethos Gris 20x60",
+                  "imgCode": "suppliers/zehavi/ethosgris.jpg",
+                  "supplierId": 1
+                },
+                {
+                  "offeringId": 15,
+                  "name": "Ethos Moka",
+                  "imgCode": "suppliers/zehavi/ethosmoka.jpg",
+                  "supplierId": 1
+                }
+              ]
+            }
+          ],
+          "videosDetails": [
+            {
+              "roomId": 2,
+              "videoLength": 50,
+              "startTime": 0,
+              "endTime": 50,
+              "videoOrdinal": 0
+            },
+            {
+              "roomId": 1,
+              "videoLength": 45,
+              "startTime": 50,
+              "endTime": 95,
+              "videoOrdinal": 1
+            },
+            {
+              "roomId": 3,
+              "videoLength": 30,
+              "startTime": 95,
+              "endTime": 125,
+              "videoOrdinal": 2
+            }
+          ],
+          "defaultFacebookVideoUrl": "https://www.facebook.com/1848414988734528/videos/1848855945357099/"
+        },
+
+
       allDesigns: {
         "title": "דירת 5 חדרים | טיפוס A1",
         "designs": [{
